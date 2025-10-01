514c491f38fe0e90184d57130e5a43576af78dc8
Lark Sigmuond A. Babao <129244163+Snorlark@users.noreply.github.com>
2025-10-01 11:50:28 +0800

    latest fix
    latest fix

--- frontend/mobile/lib/core/config/environment.dart
+++ frontend/mobile/lib/core/config/environment.dart
@@ -1,7 +1,7 @@
 enum Environment { development, staging, production }
 
 class AppConfig {
-  static Environment _environment = Environment.production;
+  static Environment _environment = Environment.development;
 
   static Environment get environment => _environment;
 
@@ -12,7 +12,7 @@ class AppConfig {
   static String get baseUrl {
     switch (_environment) {
       case Environment.development:
-        return 'https://maize-watch-app.onrender.com';
+        return 'http://10.145.164.206:8080';
       case Environment.staging:
         return 'https://maize-watch-app.onrender.com';
       case Environment.production:
